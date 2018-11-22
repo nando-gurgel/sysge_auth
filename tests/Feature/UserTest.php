c431d558bd4fbcafbf8169d0b03d239c237e729d
nando-gurgel <nando-gurgel@users.noreply.github.com>
2018-11-22 12:19:23 -0200

    1

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -3,19 +3,19 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class UserTest extends TestCase
 {
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+     public function testBasicTest()
+   {
+       $this->assertTrue(true);
+   }
 }
